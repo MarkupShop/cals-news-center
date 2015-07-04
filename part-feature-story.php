eb48197928a56fb96f5164fc70d1f7438498ef5b
Joshua Rucker <ifiamblue@gmail.com>
2015-07-03 21:20:11 -0400

    add support for video features

--- part-feature-story.php
+++ part-feature-story.php
@@ -2,7 +2,14 @@
 
     <div class="feature-story-image">
 
-        <?php if (get_field('feature-image')) : ?>
+        <?php if (get_field('webm') || get_field('mpeg')) : ?>
+            <div class="story-grid-item-image">
+                <video autoplay loop aria-hidden="true">
+                    <source src="<?php the_field('webm'); ?>" type="video/webm" />
+                    <source src="<?php the_field('mpeg'); ?>" type="video/mp4" />
+                </video>
+            </div>
+        <?php elseif (get_field('feature-image')) : ?>
             <?php $image = get_field('feature-image'); ?>
             <div class="story-grid-item-image">
                 <img src="<?php echo $image['url'] ?>" alt="<?php echo $image['alt']; ?>" title="<?php echo $image['title']; ?>" />
